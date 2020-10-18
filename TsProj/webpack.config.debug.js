5bc0cf0ca5f81836baa54b543955beb6f10d4623
Newrow <niurow@qq.com>
2020-10-18 18:03:46 +0800

    调整

--- TsProj/webpack.config.debug.js
+++ TsProj/webpack.config.debug.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 /** 忽略编辑的第三方库 */
 const externals = {
-	csharp: 'csharp',
-	puerts: 'puerts',
+	csharp: 'commonjs2 csharp',
+	puerts: 'commonjs2 puerts',
 };
 
-let local = {
+module.exports = {
 	entry: './src/GameMain.ts',
 	devtool: 'inline-source-map',
 	mode: 'development',
@@ -19,7 +19,6 @@ let local = {
 			}
 		]
 	},
-
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js', '.txt'],
 	},
@@ -27,32 +26,5 @@ let local = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, './output')
 	},
-
 	externals
 };
-
-let unity = {
-	entry: './src/GameMain.ts',
-	mode: 'development',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js', '.txt'],
-	},
-	output: {
-		filename: 'bundle.js.txt',
-		path: path.resolve(__dirname, '../Assets/Js/Resources')
-	},
-
-	externals
-};
-
-module.exports = [local, unity];
