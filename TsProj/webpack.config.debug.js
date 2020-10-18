8623c6e61d1112ba22f09abd43b0adcd172fb82f
Newrow <niurow@qq.com>
2020-10-18 17:48:24 +0800

    调整

--- TsProj/webpack.config.debug.js
+++ TsProj/webpack.config.debug.js
@@ -1,13 +1,12 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 /** 忽略编辑的第三方库 */
 const externals = {
-	csharp: 'commonjs2 csharp',
-	puerts: 'commonjs2 puerts',
+	csharp: 'csharp',
+	puerts: 'puerts',
 };
 
-module.exports = {
+let local = {
 	entry: './src/GameMain.ts',
 	devtool: 'inline-source-map',
 	mode: 'development',
@@ -21,28 +20,40 @@ module.exports = {
 		]
 	},
 
-	optimization: {    // 1. 这个配置必须
-		minimize: false
-	},
-
 	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
+		extensions: ['.tsx', '.ts', '.js', '.txt'],
 	},
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, './output')
 	},
 
-	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './output/bundle.js',
-					to: '../Assets/Js/Resources/bundle.js.txt',
-					force: true,
-				},
-			]
-		})
-	],
 	externals
 };
+
+let unity = {
+	entry: './src/GameMain.ts',
+	devtool: 'inline-source-map',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.txt'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../Assets/Js/Resources')
+	},
+
+	externals
+};
+
+module.exports = [local, unity];
--- TsProj/webpack.config.release.js
+++ TsProj/webpack.config.release.js
@@ -19,7 +19,7 @@ module.exports = {
 		]
 	},
 	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
+		extensions: ['.tsx', '.ts', '.js', '.txt']
 	},
 	output: {
 		filename: 'bundle.js',
