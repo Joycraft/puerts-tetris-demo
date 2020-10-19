ac80b0ba242fa8d337e1c079c806523dcdb7e5ee
Newrow <niurow@qq.com>
2020-10-19 10:59:39 +0800

    调整

--- TsProj/src/puerts/componentMgr.ts
+++ TsProj/src/puerts/componentMgr.ts
@@ -23,9 +23,8 @@ export class componentMgr {
     }
 
     getComponent<T extends component>(go: UnityEngine.GameObject, compType: { prototype: T }): T {
-        let hashCode = go.GetHashCode();
-        if (this.map[hashCode] == null || this.map[hashCode].length <= 0) return null;
-        let components = this.map[hashCode].filter(comp => comp instanceof <typeof component><unknown>compType);
+        let components = this.getComponents(go, compType);
+        if (components.length <= 0) return null;
         return <T>components[0];
     }
 
