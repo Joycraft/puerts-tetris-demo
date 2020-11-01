42c7da91f1cfeaabb9ddd7183ec469951b0a44ac
Newrow <niurow@qq.com>
2020-11-01 20:46:18 +0800

    调整

--- Assets/Scripts/Puerts/JsBehaviour.cs
+++ Assets/Scripts/Puerts/JsBehaviour.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public delegate void ModuleInit(JsBehaviour monoBehaviour);
 
-//只是演示纯用js实现MonoBehaviour逻辑的可能，
-//但从性能角度这并不是最佳实践，会导致过多的跨语言调用
 public class JsBehaviour : MonoBehaviour
 {
     public string ModuleName;//可配置加载的js模块
