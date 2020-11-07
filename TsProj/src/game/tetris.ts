1f150069cd2652e1126332ffe05f881757ec491a
Newrow <niurow@qq.com>
2020-11-07 19:30:08 +0800

    调整

--- TsProj/src/game/tetris.ts
+++ TsProj/src/game/tetris.ts
@@ -1,19 +1,23 @@
 import { component } from "../puerts/component";
-import { JsBehaviour } from "csharp";
+import { JsBehaviour, UnityEngine } from "csharp";
 import { common } from "../common/common";
 import { resLoader } from "../unity/resLoader";
 
 @common.globalObject
 export class tetris extends component {
+
+    content: UnityEngine.Transform = null;
+
     constructor(mono: JsBehaviour) {
         super(mono);
     }
 
     Start() {
         super.Start();
+        this.content = this.transform.Find('content');
         console.log('tetris gameLogic start.');
         let cubePrefab = resLoader.ins.loadPrefab('prefab/Cube');
-        let cube = this.Instantiate(cubePrefab, this.transform);
+        let cube = this.Instantiate(cubePrefab, this.content);
     }
 
     Update() {
