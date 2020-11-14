b6648ddb87ba928b699e6a28085532adc6fb251a
Newrow <niurow@qq.com>
2020-11-14 17:12:31 +0800

    方块定义

--- TsProj/src/game/tetris.ts
+++ TsProj/src/game/tetris.ts
@@ -1,7 +1,84 @@
 import { component } from "../puerts/component";
 import { JsBehaviour, UnityEngine } from "csharp";
 import { common } from "../common/common";
-import { resLoader } from "../unity/resLoader";
+
+interface tetrisPiece {
+    x: number,
+    y: number,
+}
+
+interface tetrisCube {
+    spins: tetrisPiece[][]
+}
+
+const tetrisCubeData: tetrisCube[] = [];
+const tetrisArrData = [];
+tetrisArrData[0] = [
+    [
+        0, 1, 0,    // ☐▨☐
+        1, 1, 1,    // ▨▨▨
+        0, 0, 0     // ☐☐☐
+    ],
+    [
+        0, 1, 0,    // ☐▨☐
+        0, 1, 1,    // ☐▨▨
+        0, 1, 0     // ☐▨☐
+    ],
+    [
+        0, 0, 0,    // ☐☐☐
+        1, 1, 1,    // ▨▨▨
+        0, 1, 0     // ☐▨☐
+    ],
+    [
+        0, 1, 0,    // ☐▨☐
+        1, 1, 0,    // ▨▨☐
+        0, 1, 0,    // ☐▨☐
+    ]
+]
+tetrisArrData[1] = [
+    [
+        1, 1, 0,    // ▨▨☐
+        1, 0, 0,    // ▨☐☐
+        1, 0, 0     // ▨☐☐
+    ],
+    [
+        0, 0, 0,    // ☐☐☐
+        1, 0, 0,    // ▨☐☐
+        1, 1, 1     // ▨▨▨
+    ],
+    [
+        0, 0, 1,    // ☐☐▨
+        0, 0, 1,    // ☐☐▨
+        0, 1, 1     // ☐▨▨
+    ],
+    [
+        1, 1, 1,    // ▨▨▨
+        0, 0, 1,    // ☐☐▨
+        0, 0, 0,    // ☐☐☐
+    ]
+]
+tetrisArrData[2] = [
+    [
+        1, 0, 0,    // ▨☐☐
+        1, 0, 0,    // ▨☐☐
+        1, 1, 0     // ▨▨☐
+    ],
+    [
+        0, 0, 0,    // ☐☐☐
+        0, 0, 1,    // ☐☐▨
+        1, 1, 1     // ▨▨▨
+    ],
+    [
+        0, 1, 1,    // ☐▨▨
+        0, 0, 1,    // ☐☐▨
+        0, 0, 1     // ☐☐▨
+    ],
+    [
+        1, 1, 1,    // ▨▨▨
+        1, 0, 0,    // ▨☐☐
+        0, 0, 0,    // ☐☐☐
+    ]
+]
 
 @common.globalObject
 export class tetris extends component {
